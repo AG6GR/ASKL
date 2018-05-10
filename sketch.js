e605cce3815ae2f8598bd35c01aaf62be898ec54
AG6GR <slhe@princeton.edu>
2018-05-10 15:54:25 -0400

    Setup code structure

--- sketch.js
+++ sketch.js
@@ -9,6 +9,28 @@ var person_pos;
 // Sprites
 var body, leg_left, leg_right;
 
+
+// ========== CALLBACKS/EVENT HANLDERS ========== //
+
+// Keyboard event handling
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    value = 255;
+  } else if (keyCode === RIGHT_ARROW) {
+    value = 0;
+  }
+}
+
+// Keyboard event handling
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    value = 255;
+  } else if (keyCode === RIGHT_ARROW) {
+    value = 0;
+  }
+}
+
+// ========== CUSTOM FUNCTIONS ========== //
 // Update all the sprite positions based on the person's location
 function updatePosition() {
   body.position.x = person_pos.x;
@@ -16,9 +38,11 @@ function updatePosition() {
 
   leg_left.position.x = person_pos.x - 200;
   leg_left.position.y = person_pos.y - 25;
+  leg_left.rotation = 45;
 
   leg_right.position.x = person_pos.x - 200;
   leg_right.position.y = person_pos.y + 25;
+  leg_right.rotation = -45;
 
 }
 
@@ -32,6 +56,7 @@ function drawBackground() {
   rect(0, h / 2, w, h / 2);
 }
 
+// ========== P5 STANDARD FUNCTIONS ========== //
 function preload() {
   person_pos = createVector(width/2, height/2);
 
@@ -49,10 +74,9 @@ function centerCanvas() {
 }
 
 function setup() {
-
-  // Background
   cnv = createCanvas(w, h);
   centerCanvas();
+  frameRate(60)
 }
 
 function draw() {
@@ -64,6 +88,7 @@ function draw() {
   // Draw the things
   drawBackground()
   drawSprites()
+  text(frameRate(), 10, 30);
 }
 
 function windowResized() {
